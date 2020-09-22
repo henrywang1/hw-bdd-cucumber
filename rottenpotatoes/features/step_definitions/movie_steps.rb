3603b0754403c74f74366814910de2d91d549fc6
henrywang1 <wanghy917@gmail.com>
2020-09-22 15:53:44 +0000

    part3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -16,7 +16,13 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2. 
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  regex = /<\s*td[^>]*>(.*?)<\s*\/\s*td>/
+  tds = page.body.scan(regex)
+  tds = tds.map { |item| item[0] }
+  expect(tds.find_index(e1)).to be < tds.find_index(e2)
+
+  # Another way:
+  # expect(/[\s\S]*#{e1}[\s\S]*#{e2}/).to match(page.body)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
